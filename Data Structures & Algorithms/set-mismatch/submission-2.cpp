a191fad8b5f635f0116b4a4cceac5e618ff4e10b
yash rathoure <93519546+Yash9658@users.noreply.github.com>
2026-06-05 11:59:51 +0530

    Add: set-mismatch - submission-2

---
+++ Data Structures & Algorithms/set-mismatch/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n = nums.size();
+        int duplicate = -1, missing = -1;
+        
+        for (int i = 1; i <= n; i++) {
+            int count = 0;
+            for (int num : nums) {
+                if (num == i) count++;
+            }
+            if (count == 2) duplicate = i;
+            else if (count == 0) missing = i;
+        }
+        
+        return {duplicate, missing};
+    }
+};
