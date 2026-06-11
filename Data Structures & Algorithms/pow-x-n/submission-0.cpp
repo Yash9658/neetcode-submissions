11bf1cc1b81b626e4bf22b99071bd039e734b119
yash rathoure <93519546+Yash9658@users.noreply.github.com>
2026-06-11 09:31:17 +0530

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == 0) return 1.0;
+        
+        long long N = n;  // Handle INT_MIN case
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        
+        double result = 1.0;
+        for (long long i = 0; i < N; i++) {
+            result *= x;
+        }
+        return result;
+    }
+};
