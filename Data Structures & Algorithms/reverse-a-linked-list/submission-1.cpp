ca6dda8aeadf2153444cc94645420439f1a53012
yash rathoure <93519546+Yash9658@users.noreply.github.com>
2026-06-13 11:44:21 +0530

    Add: reverse-a-linked-list - submission-1

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        // Base case: empty list or single node
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+        
+        // Recursively reverse the rest of the list
+        ListNode* newHead = reverseList(head->next);
+        
+        // Reverse the link between head and head->next
+        head->next->next = head;
+        head->next = nullptr;
+        
+        return newHead;
+    }
+};
