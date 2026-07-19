9fc9f6fb32d815f253900e1c443e06ce23323948
Yash Rathoure <93519546+Yash9658@users.noreply.github.com>
2026-07-19 19:55:46 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int profit=0;
+        int minele = prices[0];
+        for(int i=1;i<n;i++){
+            profit = max(profit,prices[i]-minele);
+            minele=min(prices[i],minele);
+        }
+        return profit;
+    }
+};
