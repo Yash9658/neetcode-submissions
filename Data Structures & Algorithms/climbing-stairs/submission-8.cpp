edc47d85a855bbba1f57fbe5f0ae7ad2f8dc27ff
Yash Rathoure <93519546+Yash9658@users.noreply.github.com>
2026-08-17 18:55:22 +0530

    Add: climbing-stairs - submission-8

---
+++ Data Structures & Algorithms/climbing-stairs/submission-8.cpp
@@ -0,0 +1,21 @@
+class Solution {
+private:
+    vector<int> memo;
+    
+    int solve(int n) {
+        if (n <= 0) return 0;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        
+        if (memo[n] != -1) return memo[n];
+        
+        memo[n] = solve(n - 1) + solve(n - 2);
+        return memo[n];
+    }
+    
+public:
+    int climbStairs(int n) {
+        memo.assign(n + 1, -1);
+        return solve(n);
+    }
+};
