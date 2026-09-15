39c87ff146e55b94121728f4ed90b11a1b170254
Yash Rathoure <93519546+Yash9658@users.noreply.github.com>
2026-09-16 03:02:39 +0530

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> m;
+        vector<vector<string>> ans;
+        for(auto  i : strs){
+            vector<int> count(26,0);
+            for(char c : i){
+                count[c-'a']++;
+            }
+            string key(26,'*');
+            for(int i=0;i<26;i++){
+                key[i]='a' + count[i];
+            }
+            m[key].push_back(i);
+        }
+
+
+        for(auto i : m){
+            ans.push_back(i.second);
+        }
+        return ans;
+    }
+};
