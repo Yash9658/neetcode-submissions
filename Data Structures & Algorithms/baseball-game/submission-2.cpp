b0d6fc1f5812e85ed1427f78f98c691a19e198cb
Yash Rathoure <93519546+Yash9658@users.noreply.github.com>
2026-09-25 18:08:34 +0530

    Add: baseball-game - submission-2

---
+++ Data Structures & Algorithms/baseball-game/submission-2.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int calPoints(vector<string>& op) {
+        stack<string> s1;
+        reverse(op.begin(), op.end());
+        for (auto i : op) {
+            s1.push(i);
+        }
+
+        vector<string> ans;
+        while (!s1.empty()) {
+            if (s1.top() == "+") {
+                if (ans.size() >= 2) {
+                    int ans2 =
+                        stoi(ans[ans.size() - 1]) + stoi(ans[ans.size() - 2]);
+                    ans.push_back(to_string(ans2));
+                }
+            } else if (s1.top() == "C") {
+                ans.pop_back();
+            } else if (s1.top() == "D") {
+                int ans2 = stoi(ans.back());
+                ans2 *= 2;
+                ans.push_back(to_string(ans2));
+            } else {
+                ans.push_back(s1.top());
+            }
+            s1.pop();
+        }
+        int res = 0;
+        for (auto i : ans) {
+            res += stoi(i);
+        }
+        return res;
+    }
+};
